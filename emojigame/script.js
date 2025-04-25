482d9f9e395682fb41d19a33f835ff1afe61a21b
Deniz <auo@tuta.io>
2025-04-25 16:29:43 +0200

    better emoji js

--- emojigame/script.js
+++ emojigame/script.js
@@ -21,12 +21,14 @@ const characters = [
 let stringLength = 10;
 let stringOne = document.getElementById("random-string");
 let stringTwo = document.getElementById("random-string-2");
+let btn = document.getElementById("btn");
 
 function randomChar() {
     let rI = Math.floor(Math.random() * characters.length);
     return characters[rI];
 }
 
+/*
 function generate() {
     let fullArray = "";
     for (let i = 0; i <= stringLength; i++) {
@@ -42,3 +44,18 @@ function generateTwo() {
     }
     stringTwo.textContent = fullArray;
 }
+
+Combining these 2 functions with en element as an argument using event listener*/
+
+function generate(el) {
+    let fullArray = "";
+    for (let i = 0; i <= stringLength; i++) {
+        fullArray += randomChar();
+    }
+    el.textContent = fullArray;
+}
+
+btn.addEventListener("click", function () {
+    generate(stringOne);
+    generate(stringTwo);
+});
